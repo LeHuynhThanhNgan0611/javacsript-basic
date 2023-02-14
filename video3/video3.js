b09cb6c6d11f5875dbf3c52e1fe5918286a003ed
LeHuynhThanhNgan0611 <114009954+LeHuynhThanhNgan0611@users.noreply.github.com>
2023-02-14 10:09:54 +0700

    chapter 3 video 3

---
+++ video3/video3.js
@@ -0,0 +1,9 @@
+let c = 5;
+let d = 5.9; 
+
+// const x = 5;
+// x = 6;
+    console.log(`Hello world from javascript`)
+
+    let x = 5, y = 6, z = 7;
+    console.log("Test string", 'x = ', x , 'y = ', y , 'z = ', z)
