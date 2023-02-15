5c003f14fd72d0e36ea6ab44148b4d22df457e73
LeHuynhThanhNgan0611 <114009954+LeHuynhThanhNgan0611@users.noreply.github.com>
2023-02-15 12:31:01 +0700

    chapter 6 video 6

---
+++ video6/video6.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML'.toUpperCase())
+
+let a = 5; // int, float, long, double
+let b = 6;
+let c = 6.9;
+let d = '5'
+let e = +d;
+console.log('e value: ', e, 'type: ', typeof e)
+console.log(a === e,'a:' ,typeof a, 'd:',typeof d)
