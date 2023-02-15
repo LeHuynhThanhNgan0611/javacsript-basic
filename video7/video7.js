8d4510124a3fa62e44d63db7f0026f3fb7b70191
LeHuynhThanhNgan0611 <114009954+LeHuynhThanhNgan0611@users.noreply.github.com>
2023-02-15 21:16:23 +0700

    chapter 7 video 7

---
+++ video7/video7.js
@@ -0,0 +1,9 @@
+console.log("Hello world from HTML")
+const a = 5;
+const b = 10;
+// console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+// console.log("Fifteen is " + a + b + " and\nnot " + (2 * a + b) + ".");
+
+// url: /hoidanit/5
+console.log(`Hỏi dân It check string: a = ${a}, b = ${b}, a + b = ${a + b}`)
+console.log(`/hoidanit/${a}`)
