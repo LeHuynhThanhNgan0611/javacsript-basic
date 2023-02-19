76402b0dd03afec1fcc4d3c2bd97766703a7bb95
LeHuynhThanhNgan0611 <114009954+LeHuynhThanhNgan0611@users.noreply.github.com>
2023-02-19 19:20:50 +0700

    chapter 10 video 10

---
+++ video10/video10.js
@@ -0,0 +1,18 @@
+console.log("Hello world from HTML")
+let a = 5;  // x <- y
+let b = '5';
+
+console.log(`compare a == b: a = ${a}, b = ${b}, result: ${a==b}`) // Khi đi làm đừng nên sử dụng 2 dấu bằng
+console.log(`compare a === b: a = ${a}, b = ${b}, result: ${a===b}`)
+
+// === type, value 
+// == value
+
+// Empty, null, undefined
+
+let c = null;
+console.log('check value: ', c)
+
+let d = ''; // nên khởi tạo 1 biến bằng empty
+d =5;
+d = ['11']
