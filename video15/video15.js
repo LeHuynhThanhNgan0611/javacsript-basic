47de6df7722e2dcfdcbbadc9438920dfb5da762f
LeHuynhThanhNgan0611 <114009954+LeHuynhThanhNgan0611@users.noreply.github.com>
2023-02-26 17:50:06 +0700

    chapter 15 video 15

---
+++ video15/video15.js
@@ -0,0 +1,10 @@
+console.log("Hello world from HTML")
+//global: định nghĩa ở bên ngoài có góc nhìn tổng quát
+// block
+
+// console.log('value x: ', x)
+// var x = 5;
+
+for (let i = 0; i < 10; i++) {
+    console.log(`>>>`)
+}
