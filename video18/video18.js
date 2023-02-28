2da1e53ad71630cc021ca90db518b808c75fecaa
LeHuynhThanhNgan0611 <114009954+LeHuynhThanhNgan0611@users.noreply.github.com>
2023-02-28 23:31:30 +0700

    chapter 18 video 18

---
+++ video18/video18.js
@@ -0,0 +1,12 @@
+console.log("Hello world from HTML");
+
+function sum(a, b) {
+    return a + b;
+}
+
+
+//Arrow function
+let sum = (a, b) => {
+    return a + b;
+}
+console.log('check sum: ', sum(6, 9))
